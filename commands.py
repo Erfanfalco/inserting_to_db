fb1d57705f383c4c8db792259cb2b481986425dc
erfanfalco <erfanlotfiniya13822013@gmail.com>
2024-08-18 15:23:43 +0330

    initial commit

---
+++ commands.py
@@ -0,0 +1,78 @@
+# commands for postgres cubing job
+
+daily_usable_credit_cmd = ('''SELECT  customer_data.branch_name, SUM(usable_credit), date_to_ge
+                    FROM customer_credit_data
+                    INNER join customer_data ON customer_data.account_number = customer_credit_data.account_number
+                    WHERE TO_DATE(customer_credit_data.date_to_ge, 'DD/MM/YYYY') = CURRENT_DATE
+                    GROUP BY ROLLUP (customer_data.branch_name, date_to_ge)
+                    ORDER BY date_to_ge;
+                    ''')
+
+
+daily_final_credit_cmd = ('''SELECT cd.branch_name, sum(fn.credit)::float as FinalCredit , fn.tr_ge_date::text
+                    FROM financial_status_data fn
+                    INNER join customer_data cd
+                    on cd.account_number = fn.account_number
+                    GROUP by cd.branch_name, fn.tr_ge_date
+                    order by branch_name, FinalCredit DESC;
+                    ''')
+
+
+daily_transactions_cmd = (''' WITH datas AS (
+                    SELECT 
+                        tr.branch_id,
+                        bool_or(tr.is_a_purchase) AS is_a_purchase,
+                        SUM(tr.amount) AS total_amount,
+                        MIN(tr.transaction_date_ge) AS earliest_transaction_date,
+                        cs.stock_ins_max_l_code
+                    FROM 
+                        public.transaction_agg_data tr
+                    INNER JOIN 
+                        customer_stock cs ON tr.dbs_account_number = cs.account_number
+                    GROUP BY 
+                        tr.branch_id, cs.stock_ins_max_l_code
+                    )
+                    SELECT 
+                        cd.branch_name,
+                        bool_or(datas.is_a_purchase) AS is_a_purchase,
+                        MIN(datas.earliest_transaction_date) AS Earliest_transaction_date,
+                        MAX(datas.total_amount) AS Total_amount,
+                        datas.stock_ins_max_l_code
+                    FROM 
+                        datas
+                    INNER JOIN 
+                        customer_data cd ON cd.branch_id = datas.branch_id
+                    GROUP BY 
+                        cd.branch_name, datas.stock_ins_max_l_code;
+                    ''')
+
+
+weekly_wage_cmd = ('''SELECT DATE_PART('week', (tr_ge_date - INTERVAL '6 days')::date) AS Week_Number,
+                    SUM(interest) AS Total_Interest,
+                    MIN(tr_ge_date) as tr_ge_date
+                    FROM public.financial_status_data 
+                    WHERE tr_ge_date <= CURRENT_DATE
+                    GROUP BY Week_Number 
+                    ORDER BY tr_ge_date DESC
+                    LIMIT 50;
+                    ''')
+
+
+# weekly queries
+weekly_wage_cube_query = 'INSERT INTO public.weekly_wage_cube (week_number, total_interest, tr_ge_date) VALUES (%s, %s, %s) ;'
+
+# daily queries
+usable_credit_cube_query = 'INSERT INTO usable_credit_cube (branch_name, sum_credit, date) VALUES (%s, %s, %s) ;'
+
+final_credit_cube_query = 'INSERT INTO final_credit_cube (branch_name, final_credit, tr_ge_date) VALUES (%s, %s, %s) ;'
+
+transaction_cube_query = 'INSERT INTO transaction_cube (branch_name, is_a_purchase, date, total_amount, stock_code) VALUES (%s, %s, %s, %s, %s) ;'
+
+
+checking_weekly_wage_cube = "SELECT * FROM public.weekly_wage_cube WHERE tr_ge_date = '{0}';"
+
+checking_usable_credit_cube = "SELECT * FROM public.usable_credit_cube WHERE date = '{0}';"
+
+checking_final_credit_cube = "SELECT * FROM public.final_credit_cube WHERE tr_ge_date = '{0}';"
+
+checking_transaction_cube = "SELECT * FROM public.transaction_cube WHERE date = '{0}';"
---
+++ insert_service.py
@@ -0,0 +1,126 @@
+from apscheduler.schedulers.blocking import BlockingScheduler
+import configparser
+import psycopg2
+import logging
+
+from commands import usable_credit_cube_query, transaction_cube_query, final_credit_cube_query, daily_usable_credit_cmd, \
+    daily_transactions_cmd, daily_final_credit_cmd, weekly_wage_cmd, weekly_wage_cube_query, \
+    checking_usable_credit_cube, checking_transaction_cube, checking_final_credit_cube, checking_weekly_wage_cube
+
+logging.basicConfig(level=logging.INFO)
+
+config = configparser.ConfigParser()
+path = r"C:\Users\erfan\Downloads\projects\ETL\Prediction\App\routers\DbInfo.ini"
+config.read(path)
+
+
+def get_data(cur, period='daily'):
+    command_map = {
+        'daily': [
+            daily_usable_credit_cmd,
+            daily_transactions_cmd,
+            daily_final_credit_cmd
+        ],
+        'weekly': [
+            weekly_wage_cmd
+        ]
+    }
+    cmd_list = command_map[period]
+    exe_list = []
+    for query in cmd_list:
+        try:
+            cur.execute(query)
+            exe_list.append(cur.fetchall())
+        except Exception as e:
+            logging.error(f"An error occurred: {e}")
+    return exe_list
+
+
+def insert_data(cur, conn, period, queries):
+
+    conn.autocommit = True
+    inserted_data = get_data(cur, period)
+    flags = is_exist(conn, inserted_data, period)
+    for que, data, flag in zip(queries, inserted_data, flags):
+        if data[0][0] is not None:
+            if not flag:
+                try:
+                    cur.executemany(que, data)
+                    logging.info("Data inserted successfully.")
+                except Exception as e:
+                    logging.error(f"An error occurred: {e}")
+                    conn.rollback()
+                    continue
+            else:
+                logging.info("Data is exist!")
+        else:
+            logging.info("one of your query return None")
+
+
+def is_exist(conn, data, period):
+    cur = conn.cursor()
+
+    command_map = {
+        'daily': [
+            checking_usable_credit_cube,
+            checking_transaction_cube,
+            checking_final_credit_cube
+        ],
+        'weekly': [
+            checking_weekly_wage_cube
+        ]
+    }
+
+    select_query = command_map[period]
+    results = []
+    for query, table in zip(select_query, data):
+        hasResponse = False
+        for row in table:
+            cur.execute(query.format(row[2]))
+            response = cur.fetchone()
+
+            if response is not None:
+                hasResponse = True
+                break
+
+        results.append(hasResponse)
+    return results
+
+
+def create_conn():
+    host = config.get('my_db', 'host')
+    dbname = config.get('my_db', 'dbname')
+    user = config.get('my_db', 'user')
+    password = config.get('my_db', 'password')
+    conn_string = f"host={host} dbname={dbname} user={user} password={password}"
+    conn = psycopg2.connect(conn_string)
+    return conn
+
+
+# def schedule_insert_job():
+#     scheduler = BlockingScheduler()
+#     conn = create_conn()
+#     with conn.cursor() as cur:
+#         scheduler.add_job(lambda: insert_data(cur, conn, 'daily',
+#                                               [usable_credit_cube_query, transaction_cube_query,
+#                                                final_credit_cube_query]), 'cron', hour=15, minute=30, second=0,
+#                           day_of_week="sat-sun-mon-tue-wed")
+#
+#         scheduler.add_job(lambda: insert_data(cur, conn, 'weekly',
+#                                               [weekly_wage_cube_query]), 'cron', hour=15, minute=30, second=0,
+#                           day_of_week="fri")
+#     try:
+#         scheduler.start()
+#     except (KeyboardInterrupt, SystemExit):
+#         pass
+
+
+if __name__ == "__main__":
+    # schedule_insert_job()
+    conns = create_conn()
+    with conns.cursor() as curs:
+        insert_data(curs, conns, 'daily',
+                    [usable_credit_cube_query, transaction_cube_query,
+                     final_credit_cube_query])
+        insert_data(curs, conns, 'weekly',
+                    [weekly_wage_cube_query])
